d22649582433fe0d1e727cd19bc44104e3676e35
h-waji <goo.hiro.w@gmail.com>
2026-08-20 14:08:45 +0900

    Portfolio Footerをコンポーネントに分離

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import PortfolioFooter from "@/components/PortfolioFooter";
 import PortfolioHeader from "@/components/PortfolioHeader";
 import ProjectsSection from "@/components/ProjectsSection";
 import TechnicalSandboxSection from "@/components/TechnicalSandboxSection";
@@ -11,13 +12,7 @@ export default function Home() {
 
       <ProjectsSection />
 
-      <footer className="px-7 pb-6 pt-16 md:px-11 md:pt-24 min-[1200px]:px-0">
-        <div className="mx-auto w-full max-w-[1120px] text-center">
-          <small className="font-heading tracking-wide">
-            © {new Date().getFullYear()} h-waji / hamltail
-          </small>
-        </div>
-      </footer>
+      <PortfolioFooter />
     </main>
   );
 }
